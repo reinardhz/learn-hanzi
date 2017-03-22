b19e7029e0ef5de3dd9522a07e8a5f3e09a417e9
reinardhz <reinardhz@gmail.com>
2017-03-22 19:32:56 +0700

    add files

--- src/main/java/com/reinard/learnhanzi/controllers/HomeController.java
+++ src/main/java/com/reinard/learnhanzi/controllers/HomeController.java
@@ -1,11 +1,32 @@
 package com.reinard.learnhanzi.controllers;
 
+import javax.json.stream.JsonParsingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 /**
  * A controller to provide experiment.
  * 
  * @author reinard.santosa
  *
  */
+@Controller
+@Scope(value = WebApplicationContext.SCOPE_REQUEST)
 public class HomeController {
+	
+	@RequestMapping(value = "/testHtml", method = RequestMethod.GET)
+	public String testHtml(){
+		return "test";
+	}
 
 }
