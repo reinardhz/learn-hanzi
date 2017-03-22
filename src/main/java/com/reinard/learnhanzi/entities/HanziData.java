719a6a6f043475498763abce620c979e8b68a6b0
reinardhz <reinardhz@gmail.com>
2017-03-22 20:10:59 +0700

    add

---
+++ src/main/java/com/reinard/learnhanzi/entities/HanziData.java
@@ -0,0 +1,42 @@
+package com.reinard.learnhanzi.entities;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+public class HanziData {
+	
+	private BigDecimal id;
+	private String hanzi;
+	private Timestamp createdDate;
+	
+	public HanziData(){
+		super();
+	}
+	
+	public BigDecimal getId() {
+		return id;
+	}
+	
+	public void setId(BigDecimal id) {
+		this.id = id;
+	}
+	
+	public String getHanzi() {
+		return hanzi;
+	}
+	
+	public void setHanzi(String hanzi) {
+		this.hanzi = hanzi;
+	}
+	
+	public Timestamp getCreatedDate() {
+		return createdDate;
+	}
+	
+	public void setCreatedDate(Timestamp createdDate) {
+		this.createdDate = createdDate;
+	}
+	
+	
+
+}
